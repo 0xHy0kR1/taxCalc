124adc01e76a09ef549cf00d6e31560bcce2a0b4
0xHy0kR1 <helloweenhell666@gmail.com>
2024-04-15 12:23:14 +0530

    remove some console.log and added alt for img

--- js/script.js
+++ js/script.js
@@ -40,28 +40,23 @@ document.addEventListener("DOMContentLoaded", function () {
 document.addEventListener("DOMContentLoaded", function () {
   const ageInputs = document.querySelectorAll(".age-input");
   const submitButton = document.getElementById("openModal");
-  console.log("submit button: ", submitButton);
 
   // function to check if the age input is selected
   function isAgeSelected() {
-    console.log("ageInputs: " + ageInputs);
     for (let i = 0; i < ageInputs.length; i++) {
       if (ageInputs[i].value !== "") {
-        console.log("age selected here");
         return true;
       }
     }
     return false;
   }
   submitButton.addEventListener("click", function () {
-    console.log("submit button clicked");
     const modal = document.getElementById("myModal");
     const overlay = document.querySelector(".overlay");
     let closebtn = document.querySelector(".closebtn");
 
     // Function to open the modal
     function openModal() {
-      console.log("hi");
       modal.classList.add("active");
       modal.classList.add("display");
       overlay.classList.add("display");
@@ -77,14 +72,11 @@ document.addEventListener("DOMContentLoaded", function () {
 
     // Check if the age input is selected
     if (!isAgeSelected()) {
-        console.log("inside tax calc if")
       const ageInputs = document.querySelectorAll(".age-input");
       const errorIcons = document.querySelectorAll(".alert-icon");
-        console.log("ageInputs: " + ageInputs)
       for (let i = 0; i < ageInputs.length; i++) {
         if (ageInputs[i].value !== "") {
           // Hide error icons
-          console.log("Hide error icons")
           errorIcons.forEach(function (icon) {
             icon.style.display = "none";
           });
@@ -93,7 +85,6 @@ document.addEventListener("DOMContentLoaded", function () {
       }
 
       // If no age is selected, show error icons
-      console.log("Show error icons")
       errorIcons.forEach(function (icon, index) {
         if (index === 2) {
           icon.style.display = "inline-block";
@@ -102,9 +93,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
       return false;
     } else {
-        console.log("inside tax calc else")
       const errorIcons = document.querySelectorAll(".alert-icon");
-      console.log("errorIcons ", errorIcons);
       errorIcons.forEach(function (icon, index) {
         if (index === 2) {
           icon.style.display = "none";
@@ -113,36 +102,27 @@ document.addEventListener("DOMContentLoaded", function () {
       overlay.addEventListener("click", closeModal);
       closebtn.addEventListener("click", closeModal);
       submitButton.addEventListener("click", openModal);
-      console.log("AGe selected");
       // Getting input values
       const grossIncomeInput = document.querySelector(".gross-income-input");
-      console.log(grossIncomeInput);
       const extraIncomeInput = document.querySelector(".add-income-input");
-      console.log(extraIncomeInput);
       const ageInput = document.querySelector(".age-input");
-      console.log(ageInput);
-      console.log("ageInput value: " + ageInput.value);
       const deductionsInput = document.querySelector(
         ".deductions-income-input"
       );
-      console.log(deductionsInput);
 
       // Calculating the overall income after deductions
       let overallIncome =
         parseFloat(grossIncomeInput.value) +
         parseFloat(extraIncomeInput.value) -
         parseFloat(deductionsInput.value);
-      console.log(overallIncome);
 
       const age = ageInput.value;
-      console.log("age: " + age);
       // Tax Calculation
       if (overallIncome <= 800000) {
-        overallIncome = "No tax";
+        overallIncome = `${overallIncome} No tax`;
       } else {
         let taxRate;
         if (age === "<40") {
-          console.log("under 40");
           taxRate = 0.3;
         } else if (age === ">= 40 & age < 60") {
           taxRate = 0.4;
@@ -160,7 +140,6 @@ document.addEventListener("DOMContentLoaded", function () {
 
       // Displaying the overallIncome of a person in a modal
       const modalTitle = document.querySelector(".sub-title");
-      console.log("modalTitle", modalTitle);
       try {
         modalTitle.textContent = `${overallIncome.toFixed(2)}`; 
       } catch (error) {
